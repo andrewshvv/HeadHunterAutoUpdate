1d4870135882fd653e9e7d9a9f9132dfa6c2bb9b
AndrewSamokhvalov <andrew.shvv@gmail.com>
2015-10-04 15:23:27 +0300

    Fix stange json behaviour

--- autoUpdate.py
+++ autoUpdate.py
@@ -13,12 +13,18 @@ cookieFileName = "cookies.txt"
 jobIdFileName = "jobs.txt"
 url = 'http://hh.ru/applicant/resumes/touch'
 
-def update(id,cookies):
-    payload = {"resume": id , "undirectable":"true"}
-    r = requests.post(url=url, data=payload,cookies=cookies)
+def update(applicant_id,cookies):
+    payload = {"resume" : applicant_id , "undirectable" : "true"}
+    
+    r = requests.post(url=url, data=payload, cookies=cookies)
+    if r.status_code == requests.codes.ok:
+        print("INFO: Job is updated")
+    else:
+        print("INFO: Something wrong is happening")
+        print("INFO: Status code: %s" % r.status_code)
+        print("INFO: Text: %s" % r.text)
 
 def cookie_file_transformation(file):
-
     with open(file, 'r') as content:
         cookieDictionary = json.load(content)
         return { record["name"]:record["value"] for record in cookieDictionary }
@@ -26,6 +32,8 @@ def cookie_file_transformation(file):
 def job_update(file):
     with open(file, 'r') as content:
         jobDictionary = json.load(content)
+        print(jobDictionary)
+
         cookies = cookie_file_transformation(cookieFileName)
         for record in jobDictionary:
             update(record["id"],cookies)
